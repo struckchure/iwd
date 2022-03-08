01e4e17797b1d4a1ac60d4abdb17499f0ce6deba
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-08 18:06:27 +0100

    updates

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/home";
 import PlayGround from "../views/playground";
---
+++ web/src/views/playground.tsx
@@ -0,0 +1,11 @@
+//no-commit
+
+import React from "react";
+
+function PlayGround() {
+  return (
+    <div>You can try random components designs here and whatever you want</div>
+  );
+}
+
+export default PlayGround;
