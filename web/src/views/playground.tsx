8d5c5522a09ee4366a6c402752c8a41bd548a457
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-08 18:09:22 +0100

    test

--- web/src/views/playground.tsx
+++ web/src/views/playground.tsx
@@ -4,7 +4,9 @@ import React from "react";
 
 function PlayGround() {
   return (
-    <div>You can try random components designs here and whatever you want</div>
+    <div>
+      <button>Hello</button>
+    </div>
   );
 }
 
