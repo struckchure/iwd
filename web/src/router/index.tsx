121aee42afa0ff39eb1ad6080f616dae862526a0
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-09 01:10:22 +0100

    removes playground

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "../views/home";
-import PlayGround from "../views/playground";
 
 export default function Index() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="playground" element={<PlayGround />} />
       </Routes>
     </BrowserRouter>
   );
