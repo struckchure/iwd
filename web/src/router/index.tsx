ba9f7fb18698ec12889584c01624e35b65e18c61
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-11 21:03:29 +0100

    updates routes

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../views/home";
-import FAQ from "../views/FAQ";
+import FAQ from "../views/faq";
+import Teams from "../views/teams";
+import Badge from "../views/badge";
+import Communities from "../views/communities";
 
 export default function Index() {
   return (
@@ -9,6 +12,9 @@ export default function Index() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="faq" element={<FAQ />} />
+        <Route path="teams" element={<Teams />} />
+        <Route path="/badge" element={<Badge />} />
+        <Route path="/communities" element={<Communities />} />
       </Routes>
     </BrowserRouter>
   );
