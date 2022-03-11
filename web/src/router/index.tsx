b30d989d4febd8135cca8b750ced3e0b8376bbbe
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-11 20:39:27 +0100

    moves faq component to views and updates router

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FAQ from "../layouts/FAQ";
 
 import Home from "../views/home";
-import Accordion from "../components/Accordion";
+import FAQ from "../views/FAQ";
 
 export default function Index() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="accordion" element={<Accordion />} />
         <Route path="faq" element={<FAQ />} />
       </Routes>
     </BrowserRouter>
---
+++ web/src/views/FAQ.tsx
@@ -0,0 +1,15 @@
+import Accordion from "../components/Accordion";
+import Container from "../components/Container";
+import BaseLayout from "../layouts/BaseLayout";
+
+function FAQ() {
+  return (
+    <BaseLayout>
+      <Container>
+        <Accordion />
+      </Container>
+    </BaseLayout>
+  );
+}
+
+export default FAQ;
