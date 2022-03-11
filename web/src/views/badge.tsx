b76d4ac5dda54674a047d8d2305ebae54718cade
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-11 21:02:51 +0100

    adds badge, communities, teams page

---
+++ web/src/views/badge.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BaseLayout from "../layouts/BaseLayout";
+
+function Badge() {
+  return (
+    <BaseLayout>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+      rerum eligendi assumenda expedita illo, ex architecto quae dolore
+      blanditiis minus non, temporibus, dignissimos sint id illum placeat sequi
+      perferendis!
+    </BaseLayout>
+  );
+}
+
+export default Badge;
