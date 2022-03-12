3945611d182857440fc2858564598bb9bf897718
Horlakz <horlakz@protonmail.com>
2022-03-12 17:45:09 +0100

    edited Faq.tsx file to fix conflict

--- web/src/views/faq.tsx
+++ web/src/views/faq.tsx
@@ -14,7 +14,6 @@ function FAQ() {
         <img
           src={get_image_url("hero-side-img.svg")}
           className="h-auto w-full object-contain"
-          alt="section image"
         />
       </div>
     </BaseLayout>
