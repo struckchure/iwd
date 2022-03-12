3c1e4cffa6da39c0d638f263931abe6626529185
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-12 21:57:38 +0100

    fixes z-index on faq page

--- web/src/views/faq.tsx
+++ web/src/views/faq.tsx
@@ -6,13 +6,13 @@ function FAQ() {
   return (
     <BaseLayout>
       <div className="flex flex-col items-start gap-0 md:flex-row">
-        <div className="w-full">
+        <div className="z-40 w-full">
           <Accordion />
         </div>
 
         <img
           src={get_image_url("hero-side-img.svg")}
-          className="absolute top-40 right-20 hidden h-auto w-2/5 object-contain opacity-30 md:block"
+          className="absolute top-40 right-20 z-10 hidden h-auto w-2/5 object-contain opacity-30 md:block"
           alt="section image"
         />
       </div>
