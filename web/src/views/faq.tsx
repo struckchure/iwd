98be7b70152eb3f5ca7edc3edf7b5ed9b6ff1c29
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-12 01:52:57 +0100

    updates faq

--- web/src/views/faq.tsx
+++ web/src/views/faq.tsx
@@ -1,11 +1,22 @@
 import Accordion from "../components/Accordion";
 import Container from "../components/Container";
 import BaseLayout from "../layouts/BaseLayout";
+import { get_image_url } from "../utils";
 
 function FAQ() {
   return (
     <BaseLayout>
-      <Accordion />
+      <div className="flex flex-col items-start gap-2 md:flex-row">
+        <div className="w-full">
+          <Accordion />
+        </div>
+
+        <img
+          src={get_image_url("hero-side-img.svg")}
+          className="h-auto w-full object-contain"
+          alt=""
+        />
+      </div>
     </BaseLayout>
   );
 }
